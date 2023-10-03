201f71c421f7d875ec6200c2d33209cd9ba41b3a
RafaZeero <rafaa99@gmail.com>
2023-10-03 20:39:11 -0300

    goroutines demo

--- src/lectures/demo/goroutines/goroutines.go
+++ src/lectures/demo/goroutines/goroutines.go
@@ -1,9 +1,45 @@
 package main
 
-import "fmt"
-import "time"
-import "unicode"
+import (
+	"fmt"
+	"time"
+	"unicode"
+)
 
 func main() {
 	data := []rune{'a', 'b', 'c', 'd'}
+
+	var capitalized []rune
+
+	capIt := func(r rune) {
+		capitalized = append(capitalized, unicode.ToUpper(r))
+		fmt.Printf("Done: %c \n", r)
+	}
+
+	for i := 0; i < len(data); i++ {
+		go capIt(data[i])
+	}
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Printf("Capitalized: %c \n", capitalized)
 }
+
+// // My implementation
+// func main() {
+// res := []rune{}
+// go func() {
+// 	for _, r := range data {
+// 		res = append(res, Capitalize(r))
+// 		fmt.Printf("Current strings: %v\n", res)
+// 	}
+// }()
+
+// time.Sleep(1 * time.Second)
+
+// fmt.Println(res)
+// }
+
+// func Capitalize(l rune) rune {
+// return unicode.ToUpper(l)
+// }
