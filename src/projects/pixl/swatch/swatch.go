a7640ee212fa491d902492c3c366ce83190120e8
RafaZeero <rafaa99@gmail.com>
2023-11-18 10:59:28 -0300

    create app layout

--- src/projects/pixl/swatch/swatch.go
+++ src/projects/pixl/swatch/swatch.go
@@ -6,6 +6,8 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
+
+	"pixl/apptype"
 )
 
 type Swatch struct {
@@ -23,11 +25,12 @@ func (s *Swatch) SetColor(c color.Color) {
 }
 
 // New Swatch
-func NewSwatch(c color.Color, swatchIndex int, clickHandler func(s *Swatch)) *Swatch {
+func NewSwatch(state *apptype.State, color color.Color, swatchIndex int, clickHandler func(s *Swatch)) *Swatch {
 	s := &Swatch{
-		Color:        c,
-		SwatchIndex:  swatchIndex,
+		Selected:     false,
+		Color:        color,
 		clickHandler: clickHandler,
+		SwatchIndex:  swatchIndex,
 	}
 	s.ExtendBaseWidget(s)
 	return s
@@ -36,10 +39,7 @@ func NewSwatch(c color.Color, swatchIndex int, clickHandler func(s *Swatch)) *Sw
 // Create renderer
 func (s *Swatch) CreateRenderer() fyne.WidgetRenderer {
 	square := canvas.NewRectangle(s.Color)
-	// square.StrokeWidth = 0
-	// square.StrokeColor = color.NRGBA(255, 255, 255)
-	// square.SetMinSize(fyne.NewSize(20, 20))
-	// square.SetPosition(fyne.NewPos(0, 0))
+
 	return &SwatchRenderer{
 		square:  *square,
 		objects: []fyne.CanvasObject{square},
